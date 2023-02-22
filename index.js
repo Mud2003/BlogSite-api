1e0baa2aacba40228b157e897388cb4964cc0626
Muditha Madusith <108977790+Mud2003@users.noreply.github.com>
2023-02-22 18:09:29 +0530

    Authentications
     50%

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/User');
+
+
 const app = express();
 
-app.get('/test', (req,res) => {
-    res.json('test ok');
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://muditha:FxJg8C0h2j2AFzl0@cluster0.mnc1whi.mongodb.net/?retryWrites=true&w=majority')
+
+app.post('/register', async (req,res) => {
+    const {username,password} = req.body;
+    const userDoc = await User.create({username, password});
+    res.json(userDoc);
 });
 
 app.listen(4000);
+
+//
